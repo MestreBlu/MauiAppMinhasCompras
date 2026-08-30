ad377c2ec7fe1868461cfd02084e979c3d35c3f6
MestreBlu <mestreblu@yahoo.com>
2026-08-30 10:10:44 -0300

    Add files via upload
    Entrega Agenda 2 DS

---
+++ AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using MauiAppMinhasCompras.Views;
+using Microsoft.Maui.Controls;
+
+namespace MauiAppMinhasCompras
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            Routing.RegisterRoute(nameof(NovoProduto), typeof(NovoProduto));
+            Routing.RegisterRoute(nameof(EditarProduto), typeof(EditarProduto));
+        }
+    }
+}
+
+
+
